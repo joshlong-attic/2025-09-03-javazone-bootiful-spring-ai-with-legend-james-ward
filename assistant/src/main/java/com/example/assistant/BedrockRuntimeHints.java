170fc959ff890270b139106d148bd464eebb9031
Josh Long <54473+joshlong@users.noreply.github.com>
2025-12-08 18:15:16 -0500

    save

--- assistant/src/main/java/com/example/assistant/BedrockRuntimeHints.java
+++ assistant/src/main/java/com/example/assistant/BedrockRuntimeHints.java
@@ -9,6 +9,7 @@ import org.springframework.aot.hint.TypeReference;
 import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.util.ClassUtils;
 
@@ -34,8 +35,58 @@ class BedrockRuntimeHints implements RuntimeHintsRegistrar {
 
 	private final Collection<TypeReference> allClasses;
 
+	private final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
 	BedrockRuntimeHints() {
 		this.allClasses = this.find(rootPackage);
+	}
+
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		try {
+			this.registerBedrockRuntimeService(hints);
+			this.registerSerializationClasses(hints);
+			this.registerResources(hints);
+		} //
+		catch (Throwable ex) {
+			log.warn("error when registering Bedrock types", ex);
+		}
+	}
+
+	private void registerBedrockRuntimeService(RuntimeHints hints) {
+		var pkg = rootPackage + ".services.bedrockruntime";
+		var all = new HashSet<TypeReference>();
+		for (var clzz : this.allClasses) {
+			if (clzz.getName().contains("Bedrock") && clzz.getName().contains("Client"))
+				all.add(clzz);
+		}
+		var modelPkg = pkg + ".model";
+		all.addAll(this.find(modelPkg));
+		all.forEach(tr -> hints.reflection().registerType(tr, this.memberCategories));
+	}
+
+	private void registerSerializationClasses(RuntimeHints hints) {
+		for (var c : this.allClasses) {
+			try {
+				var serializableClass = ClassUtils.forName(c.getName(), getClass().getClassLoader());
+				if (Serializable.class.isAssignableFrom(serializableClass)) {
+					hints.reflection().registerType(serializableClass, this.memberCategories);
+					hints.serialization().registerType(c);
+				}
+			} //
+			catch (Throwable e) {
+				//
+			}
+		}
+	}
+
+	private void registerResources(RuntimeHints hints) throws Exception {
+		for (var resource : this.resolver.getResources("classpath*:software/amazon/awssdk/**/*.interceptors")) {
+			hints.resources().registerResource(resource);
+		}
+		for (var resource : this.resolver.getResources("classpath*:software/amazon/awssdk/**/*.json")) {
+			hints.resources().registerResource(resource);
+		}
 	}
 
 	protected List<TypeReference> find(String packageName) {
@@ -60,64 +111,5 @@ class BedrockRuntimeHints implements RuntimeHintsRegistrar {
 			.toList();
 
 	}
-
-	@Override
-	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-		registerBedrockRuntimeService(hints, classLoader);
-		registerSerializationClasses(hints, classLoader);
-		registerResources(hints);
-	}
-
-	private void registerBedrockRuntimeService(RuntimeHints hints, ClassLoader classLoader) {
-		var pkg = rootPackage + ".services.bedrockruntime";
-		var all = new HashSet<TypeReference>();
-		for (var clzz : this.allClasses) {
-			if (clzz.getName().contains("Bedrock") && clzz.getName().contains("Client"))
-				all.add(clzz);
-		}
-		var modelPkg = pkg + ".model";
-		all.addAll(this.find(modelPkg));
-		all.forEach(tr -> hints.reflection().registerType(tr, this.memberCategories));
-	}
-
-	private void registerSerializationClasses(RuntimeHints hints, ClassLoader classLoader) {
-		for (var c : this.allClasses) {
-			try {
-				var clzz = ClassUtils.forName(c.getName(), getClass().getClassLoader());
-				if (Serializable.class.isAssignableFrom(clzz)) {
-					// this.register(hints, classLoader, clzz.getName());
-					hints.reflection().registerType(clzz, this.memberCategories);
-					hints.serialization().registerType(c);
-				}
-			} //
-			catch (Throwable e) {
-				//
-			}
-		}
-	}
-
-	private void registerResources(RuntimeHints hints) {
-		// Service model resources
-		hints.resources().registerPattern("software/amazon/awssdk/services/bedrock*/codegen-resources/service-2.json");
-		hints.resources()
-			.registerPattern("software/amazon/awssdk/services/bedrock*/codegen-resources/endpoint-rule-set.json");
-		hints.resources()
-			.registerPattern("software/amazon/awssdk/services/bedrock*/codegen-resources/paginators-1.json");
-		hints.resources().registerPattern("software/amazon/awssdk/services/bedrock*/codegen-resources/waiters-2.json");
-
-		// Partition data
-		hints.resources().registerPattern("software/amazon/awssdk/global/handlers/execution.interceptors");
-	}
-
-	/*
-	 * private void register(RuntimeHints hints, ClassLoader classLoader, String
-	 * className) { for (var clzz : new String[] { className, className + "$Builder",
-	 * className + "$BuilderImpl", className + "$SerializableBuilder" }) { try { var clazz
-	 * = Class.forName(clzz, false, classLoader);
-	 * hints.reflection().registerType(TypeReference.of(clazz), memberCategories); } //
-	 * catch (Throwable e) { this.log.trace("Failed to register Bedrock class {}",
-	 * className, e); } } }
-	 *
-	 */
 
 }
